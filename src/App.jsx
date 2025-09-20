46573eb453801ed3ddb4a836c7f12542eda2984d
anggathestarboy <anggarathestarboy@gmail.com>
2025-09-20 10:41:44 +0700

    updateting

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
+import "./app.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [cars, setCars] = useState([]);
+	const [id, setId] = useState(1);
+  const [car, setCar] = useState({});
 
-	const fetchCars = async () => {
-		const response = await fetch('https://64d358c767b2662bf3dc103e.mockapi.io/api/v1/cars');
-		const data = await response.json();
-		setCars(data);
+	const onNext = () => {
+		const nextId = id + 1;
+		if (nextId > 10) {
+			return setId(1);
+		}
+
+		return setId(nextId);
 	}
 
-	// akan dijalankan ketika pertama kali di-render
+	const fetchCar = async () => {
+		const response = await fetch('https://64d358c767b2662bf3dc103e.mockapi.io/api/v1/cars/' + id);
+		const data = await response.json();
+		setCar(data);
+	}
+	
+// Akan dijalankan ketika pertama kali di-render dan setiap kali terdapat perubahan state id
 	useEffect(() => {
-		fetchCars();
-	}, []);
+		fetchCar();
+	}, [id]);
 
   return (
-    <ul>
-			{
-				cars.map((car) => (
-					<li>{ car.name }</li>
-				))
-			}
-    </ul>
+    <div>
+			<p>{ car.name }</p>
+			<button onClick={onNext}>Selanjutnya</button>
+    </div>
   );
 }
 
